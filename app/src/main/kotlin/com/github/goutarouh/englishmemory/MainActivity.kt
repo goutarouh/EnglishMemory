3c65463bbc362d872d82381463071f6dbb10e0ae
goutarouh <gouta.gorogoro@gmail.com>
2023-04-04 22:51:01 +0900

    #4 画面が消えないように修正

--- app/src/main/kotlin/com/github/goutarouh/englishmemory/MainActivity.kt
+++ app/src/main/kotlin/com/github/goutarouh/englishmemory/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.goutarouh.englishmemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             MaterialTheme {
                 Surface {
